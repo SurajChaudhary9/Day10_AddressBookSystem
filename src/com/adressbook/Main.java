56dd4065533f3e7d9d5a09dd834ada14a1e21a23
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-23 20:01:17 +0530

    Added : Ability to add Multiple contacts in address book

---
+++ src/com/adressbook/Main.java
@@ -0,0 +1,12 @@
+package com.adressbook;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to the address book system");
+	AddressBookMain addressBookMain = new AddressBookMain();
+	addressBookMain.addMultiplePerson();
+
+    }
+
+}
