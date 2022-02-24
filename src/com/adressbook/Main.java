8b676fa1773017c9e63d996bae40a29ac2b34e83
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-24 09:19:55 +0530

    Refactored : main

--- src/com/adressbook/Main.java
+++ src/com/adressbook/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	HashMap<String, AddressBookMain> addressbook = new HashMap<>();
 
 	/*
-	 * Ability to add multiple addressbooks
+	 * ablility to add multiple addressbook
 	 * 
 	 */
 
@@ -16,13 +16,15 @@ public class Main {
 		while (true) {
 
 			System.out.println("Enter selection \n1)To Access adress book\n2)exit and add");// add new if running for
-																							// first time
+																							// fort
 			int selection = sc.nextInt();
 			switch (selection) {
+
 			case 1:
+				System.out.println("enter adress book name");
 				String name = sc.next();
 				if (addressbook.containsKey(name)) {
-					System.out.println("Entered addressbook name already present");
+					System.out.println("entered addressbook name already present");
 				} else {
 					AddressBookMain addr = new AddressBookMain();
 					addr.addMultiplePerson();
